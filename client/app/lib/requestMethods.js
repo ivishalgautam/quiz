3370ef8fb7a288da1268fcb0d72a31907806a21c
Vishal Gautam <76483761+ivishalgautam@users.noreply.github.com>
2023-09-27 23:46:17 +0530

    initial

--- client/app/lib/requestMethods.js
+++ client/app/lib/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { getCookie } from "./cookies";
 
-const BASE_URL = "http://localhost:4000/api";
-// const BASE_URL = "https://nla-server-production.up.railway.app/api";
+// const BASE_URL = "http://localhost:4000/api";
+const BASE_URL = "https://nla-server-production.up.railway.app/api";
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
